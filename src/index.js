ff04828368a23bd2ad22e043144c57e3f900f5aa
Alessandro Catania <alessandro.catania@concirrus.com>
2019-11-10 02:44:31 +0000

    1.0.0

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const cmd = require("node-cmd");
+const Promise = require("bluebird");
+const minimist = require("minimist");
+
+const getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd });
+const parsedParameters = minimist(process.argv.slice(2));
+
+let lintCommand =
+    typeof parsedParameters.l === "string" ? parsedParameters.l : "node -v"; // -n <componentName> / OPTIONAL
+console.log(lintCommand);
+getAsync(lintCommand).then(() => {
+    getAsync("git diff-files").then(data => {
+        console.log('data', data);
+        if (data && data[0].length > 0) {
+            console.log("There were linting errors or there are untracked files, stage them and commit again.");
+        } else {
+            let commitMessage =
+                typeof parsedParameters.m === "string" ? parsedParameters.m : undefined; // -n <componentName> / OPTIONAL
+            if (commitMessage !== undefined) {
+                getAsync(`git commit -m "${commitMessage}"`).then(() => {
+                    getAsync("npm version patch").then(() => {
+                        getAsync("git add package.json").then(() => {
+                            console.log("version updated");
+                        });
+                    });
+                });
+            } else {
+                console.warn("Missing commit message");
+            }
+        }
+    });
+});
