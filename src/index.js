c61ac4110518004fa73ab05c0ff75338851cfc05
Alessandro Catania <alessandro.catania@concirrus.com>
2019-11-10 03:04:25 +0000

    Added console.log

--- src/index.js
+++ src/index.js
@@ -6,30 +6,36 @@ const getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd });
 const parsedParameters = minimist(process.argv.slice(2));
 console.log(parsedParameters);
 let lintCommand =
-    typeof parsedParameters.l === "string" ? parsedParameters.l : "node -v"; // -n <componentName> / OPTIONAL
+  typeof parsedParameters.l === "string" ? parsedParameters.l : "node -v"; // -n <componentName> / OPTIONAL
 console.log(lintCommand);
 getAsync(lintCommand).then(() => {
-    getAsync("git diff-files").then(data => {
-        if (data && data[0].length > 0) {
-            console.log("There were linting errors or there are untracked files, stage them and commit again.");
-        } else {
-            let commitMessage =
-                typeof parsedParameters.m === "string" ? parsedParameters.m : undefined; // -n <componentName> / OPTIONAL
-            if (commitMessage !== undefined) {
-                getAsync(`git commit -m "${commitMessage}"`).then(() => {
-                    if (parsedParameters.major) {
-                        getAsync("npm version major").then(() => { console.log('Major version released'); });
-                    } else if (parsedParameters.minor) {
-                        getAsync("npm version minor").then(() => { console.log('Minor version released'); });
-                    } else {
-                        getAsync("npm version patch").then(() => {
-                            console.log("version updated");
-                        });
-                    }
-                });
-            } else {
-                console.warn("Missing commit message");
-            }
-        }
-    });
+  getAsync("git diff-files").then(data => {
+    if (data && data[0].length > 0) {
+      console.log(
+        "There were linting errors or there are untracked files, stage them and commit again."
+      );
+    } else {
+      let commitMessage =
+        typeof parsedParameters.m === "string" ? parsedParameters.m : undefined; // -n <componentName> / OPTIONAL
+      if (commitMessage !== undefined) {
+        getAsync(`git commit -m "${commitMessage}"`).then(() => {
+          if (parsedParameters.major) {
+            getAsync("npm version major").then(() => {
+              console.log("Major version released");
+            });
+          } else if (parsedParameters.minor) {
+            getAsync("npm version minor").then(() => {
+              console.log("Minor version released");
+            });
+          } else {
+            getAsync("npm version patch").then(() => {
+              console.log("version updated");
+            });
+          }
+        });
+      } else {
+        console.warn("Missing commit message");
+      }
+    }
+  });
 });
